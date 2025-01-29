aa2a10befdd3997bcfd279fe889cc9d6bba815bb
Breno Paiva <breno_p@tuta.io>
2025-01-29 16:57:35 -0300

    [configuring] (gamemode.sh) refactor

--- scripts/gamemode.sh
+++ scripts/gamemode.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env sh
 
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+HYPRGAMEMODE=$(hyprctl getoption decoration:blur:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
-  #killall waybar
   hyprctl --batch "\
-        keyword animations:enabled 0;\
-	keyword decoration:active_opacity 1.0;\
-	keyword decoration:inactive_opacity 1.0;\
-        keyword decoration:drop_shadow 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+    keyword animations:enabled 0;\
+    keyword decoration:active_opacity 1.0;\
+    keyword decoration:inactive_opacity 1.0;\
+    keyword decoration:drop_shadow 0;\
+    keyword decoration:blur:enabled 0;\
+    keyword general:gaps_in 0;\
+    keyword general:gaps_out 0;\
+    keyword general:border_size 1;\
+    keyword decoration:rounding 0"
+
+  #killall waybar
+
   exit
 fi
 hyprctl reload
