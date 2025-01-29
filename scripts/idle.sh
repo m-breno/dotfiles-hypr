2f26a9e5cc5f6d35777703bfc876dc24a94b8b05
Breno Paiva <breno_p@tuta.io>
2025-01-29 16:59:06 -0300

    [configuring] remove unecessary scripts

--- scripts/idle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-swayidle -w \
-  before-sleep "playerctl pause" \
-  timeout 120 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on" \
-  timeout 135 "$XDG_CONFIG_HOME/hypr/scripts/lock.sh" \
-  timeout 300 "systemctl suspend"
